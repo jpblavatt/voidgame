738b60fa752858a56a85d8521698718c82a18858
Josh Blavatt <jpblavatt@enochwalk.com>
2021-11-30 09:09:12 -0500

    Version 1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
 	name="voidgame",
-	version="1.0",
+	version="1.1",
 	author="Joshua Blavatt",
 	author_email="josh@joshuablavatt.dev",
 	description="A simple interface for a unique game",
